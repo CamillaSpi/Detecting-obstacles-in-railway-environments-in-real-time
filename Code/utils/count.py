929dd1b77906f69c9f39164e0636a4c097ee2a90
CamillaSpi <94001531+CamillaSpi@users.noreply.github.com>
2024-06-23 10:24:25 +0200

    Delete Code directory

--- Code/utils/count.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path_where_search = "/user/cspingola/TESI/Tesi/TestSets/TestSetsV1/TestSetInternetV1/source_images"
-
-count = 0
-for elem in os.listdir(path_where_search):
-    #if elem.endswith(".png"):
-    count+=1
-print("count", count)
